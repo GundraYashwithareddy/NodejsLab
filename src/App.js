694c023607e74e793dad9555658d122cadfff712
Yashwitha Reddy <24wh5a1207@bvrithyderabad.edu.in>
2025-04-13 08:54:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ function App() {
   return (
     <div className="resume">
       <header className="header">
-        <h1>Gundra Yashwithareddy</h1>
-        <p>yashwithareddygundra@gmail.com | +91-7569341570</p>
+        <h1>Gujarvadi Sravanthi</h1>
+        <p>sravanthighujarvad@gmail.com| +91-8309788285</p>
       </header>
 
       <section>
         <h2>About Me</h2>
-        <p> I’m a passionate React developer focused on building modern web applications using React, Node.js, and databases.</p>
+        <p> An IT undergraduate with a strong foundation in coding, including Java and Data Structures & Algorithms, passionate about solving problems and building practical software solutions.
+
+</p>
       </section>
 
       <section>
@@ -23,30 +25,26 @@ function App() {
       <section>
         <h2>TechnicalSkills</h2>
         <ul>
-          <li>Programming languages :Java,C.</li>
-          <li>Web tecnologies :HTML, CSS, Bootstrap</li>
-          <li>Data bases :MySQL</li>
-          <li>Design Tools: AutoCAD, SketchUp</li>
-          <li>Git, GitHub</li>
+          <li>Java,C,Python.</li>
+          <li>HTML, CSS, Bootstrap</li>
+          <li>MySQL</li>
         </ul>
       </section>
 
       <section>
         <h2>SoftSkills</h2>
-          <p> Communication, Logicalthinking, Teamwork, Creative</p>
+          <p> Adaptability , ProblemSolving, Creative</p>
       </section>
 
       <section>
         <h2>Projects</h2>
-        <p><strong>Online Quiz System </strong> Developed a java based online quiz platfrom using oops principles .features included user login , question management and scoring </p>
+        <p><strong>Nivi Chat</strong> </p>
       </section>
 
       <section>
         <h2>Certifictions</h2>
         <ul>
           <li>Great AppSec Hackathon 2024</li>
-          <li>DevAstra 2025 - A National level 24 hours Hackathon</li>
-          <li>AutoCAD </li>
         </ul>
       </section>
     </div>
